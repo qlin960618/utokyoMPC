e7eb7f1e3fa6f224cb4fde1dc5a147df0e820862
qlin960618 <31228437+qlin960618@users.noreply.github.com>
2021-07-29 18:15:21 +0900

    Running compact again...

--- run/go_21.sh
+++ run/go_21.sh
@@ -9,7 +9,7 @@
 #PJM -e err
 #PJM -o core42.lst
 
-export KMP_AFFINITY=granularity=fine,balanced
+export KMP_AFFINITY=granularity=fine,compact
 ./sol20
 ./sol20
 ./sol20
--- run/go_23.sh
+++ run/go_23.sh
@@ -9,7 +9,7 @@
 #PJM -e err
 #PJM -o core46.lst
 
-export KMP_AFFINITY=granularity=fine,balanced
+export KMP_AFFINITY=granularity=fine,compact
 ./sol20
 ./sol20
 ./sol20
