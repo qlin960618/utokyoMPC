37113e52885f98b0266137a46d339629106bb206
qlin960618 <31228437+qlin960618@users.noreply.github.com>
2021-07-29 15:55:08 +0900

    new run

--- run/go_5.sh
+++ run/go_5.sh
@@ -2,14 +2,14 @@
 #PJM -N "test1"
 #PJM -L rscgrp=lecture
 #PJM -L node=1
-#PJM --omp thread=112
+#PJM --omp thread=46
 #PJM -L elapse=00:15:00
 #PJM -g gt69
 #PJM -j
 #PJM -e err
-#PJM -o core112.lst
+#PJM -o core46.lst
 
-export KMP_AFFINITY=granularity=fine,balanced
+export KMP_AFFINITY=granularity=fine,compact
 ./sol20
 ./sol20
 ./sol20
