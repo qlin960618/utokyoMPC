663bc42acf27dd6721f6cf67a7ced99a8048ca4b
qlin960618 <31228437+qlin960618@users.noreply.github.com>
2021-08-02 16:23:11 +0900

    making loop control shared

--- src20_FTDP/solver_PCG.c
+++ src20_FTDP/solver_PCG.c
@@ -45,7 +45,7 @@ solve_PCG (int N, int NL, int NU, int *indexL, int *itemL, int *indexU, int *ite
 	BNRM2 = 0.0;
 	*ITR = N;
 	// might need to start earlier in the initialization of pointer
-	#pragma omp parallel shared(Stime, Etime, N, ERR) private(i)
+	#pragma omp parallel shared(Stime, Etime, N, ERR, L, *ITR) private(i)
 	{
 	/* initializationunder this block
 		#pragma omp for private (i)
