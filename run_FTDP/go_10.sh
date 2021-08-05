40face79cb5b5d472a0938ca0af15c7f474d4562
qlin960618 <31228437+qlin960618@users.noreply.github.com>
2021-08-05 20:06:02 +0900

    run FTDP balanced

---
+++ run_FTDP/go_10.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#PJM -N "test1"
+#PJM -L rscgrp=lecture
+#PJM -L node=1
+#PJM --omp thread=20
+#PJM -L elapse=00:15:00
+#PJM -g gt69
+#PJM -j
+#PJM -e err
+#PJM -o core20.lst
+
+export KMP_AFFINITY=granularity=fine,balanced
+./sol20
+./sol20
+./sol20
+./sol20
+./sol20
